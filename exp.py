96f62b284cfb1e17a399a50023f0ba4e03a4f50f
eunicen1 <ee.nwaobi@gmail.com>
2021-11-20 19:40:30 -0800

    leakypeakdetection

--- exp.py
+++ exp.py
@@ -6,49 +6,79 @@ from wfdb import processing as prc
 
 record_ca = wfdb.rdrecord('mitbih/420') 
 record_normal = wfdb.rdrecord('ecgiddb/Person_01/rec_1')
-# cdcarrest = np.array(record_ca[0])
-# normal = np.array(record_normal[0])
-
-# knorm = normal[:,0]
 print(record_ca.__dict__['fs'])
 print(record_ca.__dict__['p_signal'])
+xca = record_ca.__dict__['p_signal'][:, 1]
+xnorm = record_normal.__dict__['p_signal'][:, 1]
 
-# print(record_normal.__dict__['fs'])
-# print(record_normal.__dict__['p_signal'])
+#Input: 2-d array [indexpeak, peakamplitude]
+#Output: 2-d array of [indexpeak, peakamplitude, ratiorelativeAvgpeak] 
+def pkRatio(arr, p=0.33):
+    #find mean of top p-th% peaks
+    n,_ = arr.shape
+    Δp = int(p*n)
+    arr2 = arr[:,1]
+    np.sort(arr2, kind='mergesort')
+    avgppeak = np.mean(arr2[n-Δp:])
+    #determine ratio of peaks 
+    ratios = 100*arr[:,1]/avgppeak
+    return np.concatenate((arr, ratios.reshape(n,1)), axis=1)
 
-xca = record_ca.__dict__['p_signal'][:, 0]
-xnorm = record_normal.__dict__['p_signal'][:, 0]
-# plt.plot(xca)
-# plt.plot(xnorm)
-# plt.legend(['c a', 'normal'])
-# plt.show()
 
-#each PQRST = example
-#PQRST has P wave, QRS wave, T wave
-#P wave brings frequency, amplitude, width
-#QRS wave brings frequency, amplitude, width
-#T wave brings frequency, amplitude, width
-#X=nx9
-# sigca, fieldsca = wfdb.rdsamp('mitbih/420')
-# caqrs = prc.gqrs_detect(sig=sigca[:,0], fs=fieldsca['fs'])
-# pks = prc.find_peaks(sig=sigca[:,0])
-# print(caqrs, pks[0])
 
-#Input = 1nd signal array
-def detectFrequency(sig):
-    n = sig.shape[0]
-    Δy = np.zeros(n)
-    for i in range(1, n-1):
-        #local maxima
-        if(sig[i]-sig[i-1]) >= 0 and (sig[i]-sig[i+1]) >= 0:
-            Δy[i] = sig[i]
-        #local minima
-        if(sig[i]-sig[i-1]) <= 0 and (sig[i]-sig[i+1]) <= 0:
-            Δy[i] = sig[i]
-    print("dely", Δy)
+#Input = 1-d signal array
+#Output: 2-d array of estimated [P-wave amplitude,
+# P-wave width from 0-0,
+# QRS-wave amplitude,
+# QRS-wave width from 0-0,
+# T-wave amplitude,
+# T-wave width from 0-0,
+# avg frequency of overall signal] = examples
+def detectPQRSTf(sig):
+    n = sig.shape[0] #retrieve signal length, n
+    neginds = [] #initialize negative index 
+    # - peaks are usually encapsulated by negative data
+    # ----- 00 ++++++ peak +++++ 00 -----
+    for i in range(n):
+        # append negative indexes to array 
+        if(sig[i] < 0):
+           neginds.append([i,sig[i]]) 
+    neginds = np.array(neginds)
+    p, _ = neginds.shape
+    maxs = []
+    for i in range(p-1):
+        start = neginds[i,0] 
+        stop = neginds[i+1,0]
+        Δ = stop - start
+        if Δ > 1: 
+            idx = range(int(start), int(stop))
+            slice = sig[int(start):int(stop)]
+            #get max and index of a potential peak
+            maxs.append([idx[np.argmax(slice)], np.max(slice)])
+    maxs = np.array(maxs)
 
+    # all maxs and relative closeness to max
+    rmaxs = pkRatio(maxs, p=2/(maxs.shape[0]))
+    r, _ = rmaxs.shape
+    QRS = []
+    for i in range(r):
+        percent = rmaxs[i,2] # obtain percentage %
+        if int(percent) >= 100:
+            QRS.append([rmaxs[i,0], rmaxs[i,1]])
+    QRS = np.array(QRS)
+
+    ##[mean frequency of P-wave, mean frequency of QRS wave, mean frequency of T wave]    
+    #fs = [np.mean(np.diff(P[:,0])), np.mean(np.diff(QRS[:,0])), np.mean(np.diff(T[:,0]))]
+    #print(fs)
+    # f = np.mean(fs) #?
+    
+    np.savetxt("neginds.csv", neginds, delimiter=",")
     plt.plot(sig)
-    plt.annotate()
+    plt.scatter(neginds[:,0], neginds[:,1], color='cyan')
+    plt.scatter(maxs[:,0], maxs[:,1], color='coral')
+    plt.scatter(QRS[:,0], QRS[:,1], color='maroon')
+    plt.show()
+    
 
     # Δymin = np.min([ np.abs(sig[i] - sig[i-1]) for i in range(1,n) ])
     # Δymax = np.max([ np.abs(sig[i] - sig[i-1]) for i in range(1,n) ])
@@ -63,7 +93,5 @@ def detectFrequency(sig):
     # print("sigmin:", sigmin)
 
 
-detectFrequency(xca)
-def pqrst(sig):
-    pass
-
+detectPQRSTf(xca)
+detectPQRSTf(xnorm)
