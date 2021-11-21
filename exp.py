4bf81b14eb179509898956bf7825183aaf68f5fa
eunicen1 <ee.nwaobi@gmail.com>
2021-11-21 08:22:24 -0800

    QRS detection with broken P, T detection

--- exp.py
+++ exp.py
@@ -13,8 +13,8 @@ xnorm = record_normal.__dict__['p_signal'][:, 1]
 
 #Runtime below: ~O(n log n + n) = ~O(n log n)
 
-#Input: 2-d array [indexpeak, peakamplitude]
-#Output: 2-d array of [indexpeak, peakamplitude, ratiorelativeAvgpeak] 
+#Input: 2-d array [indexpeak, peakamp]
+#Output: 2-d array of [indexpeak, peakamp, ratiorel2Avgpeak] 
 def pkRatio(arr, p=0.33):
     print("top "+str(p*100.0)+"%-peaks")
     #find mean of top p-th% peaks
@@ -22,22 +22,19 @@ def pkRatio(arr, p=0.33):
     Δp = int(p*n)
     arr2 = np.copy(arr[:,1])
     arr2.sort(kind='mergesort') # sorts array in place
-    print(arr2[n-Δp:])
-    print(np.max(arr2))
     avgppeak = np.mean(arr2[n-Δp-1:])
     #determine ratio of peaks 
     ratios = 100*arr[:,1]/avgppeak
-    print(ratios)
     return np.concatenate((arr, ratios.reshape(n,1)), axis=1)
 
 #Input = 1-d signal array
-#Output: 2-d array of estimated [P-wave amplitude,
+#Output: 2-d array of average [P-wave amplitude,
 # P-wave width from 0-0,
 # QRS-wave amplitude,
 # QRS-wave width from 0-0,
 # T-wave amplitude,
 # T-wave width from 0-0,
-# avg frequency of overall signal] = examples
+# frequency of overall signal] = examples by IID
 def detectPQRSTf(sig):
     n = sig.shape[0] #retrieve signal length, n
     neginds = [] #initialize negative index 
@@ -70,29 +67,35 @@ def detectPQRSTf(sig):
     for i in range(r):
         percent = rmaxs[i,2] # obtain percentage %
         if int(percent) >= 100:
-            QRS.append([rmaxs[i,0], rmaxs[i,1]])
+            QRS.append([rmaxs[i,0], rmaxs[i,1], i])
+            print(i)
     QRS = np.array(QRS)
     s, _ = QRS.shape
 
-    #get estimated P, T peaks
+    #BROKEN!!!!!!!!!!!!
+    #get estimated P, T, QRS(mod) peaks 
     P = []
     T = []
+    QRSnew = []
     for i in range(1,s-1):
-        startP = int(QRS[i-1,0])
-        stopP = int(QRS[i,0])
-        startT = int(QRS[i,0])
-        stopT = int(QRS[i+1,0])
+        startP = int(QRS[i-1,2])
+        stopP = int(QRS[i,2])
+        startT = int(QRS[i,2])
+        stopT = int(QRS[i+1,2])
         print(startP, stopP, startT, stopT)
-        idxP = range(startP, stopP)
-        idxT = range(startT, stopT)
-        sliceP = maxs[startP+1:stopP,1]
-        sliceT = maxs[startT+1:stopT,1]
-        print(sliceP.shape, sliceT.shape)
+        sliceP = rmaxs[startP+1:stopP,1]
+        sliceT = rmaxs[startT+1:stopT,1]
+        idxP = range(int(QRS[i-1,0]), int(QRS[i,0]))
+        idxT = range(int(QRS[i,0]), int(QRS[i+1,0]))
         #most significant peak (max in range between 2 QRS peaks)
-        P.append([idxP[np.argmax(sliceP)], np.max(sliceP)])
-        T.append([idxT[np.argmax(sliceT)], np.max(sliceT)])
+        if sliceP.shape[0] > 0 and sliceT.shape[0] > 0: #should we remove QRS as well?
+            print("index of max p", sig[idxP[np.argmax(sliceP)]], idxP[np.argmax(sliceP)], np.max(sliceP))
+            QRSnew.append(QRS[i,:])
+            P.append([idxP[np.argmax(sliceP)], np.max(sliceP)])
+            T.append([idxT[np.argmax(sliceT)], np.max(sliceT)])
     P = np.array(P)
     T = np.array(T)
+    QRS = np.array(QRSnew)
     print(P, T)
     ##[mean frequency of P-wave, mean frequency of QRS wave, mean frequency of T wave]    
     #fs = [np.mean(np.diff(P[:,0])), np.mean(np.diff(QRS[:,0])), np.mean(np.diff(T[:,0]))]
@@ -105,22 +108,9 @@ def detectPQRSTf(sig):
     plt.scatter(maxs[:,0], maxs[:,1], color='coral')
     plt.scatter(QRS[:,0], QRS[:,1], color='maroon')
     plt.scatter(P[:,0], P[:,1], color='lime')
-    plt.scatter(T[:,0], T[:,1], color='majenta')
+    plt.scatter(T[:,0], T[:,1], color='red')
     plt.show()
     
-
-    # Δymin = np.min([ np.abs(sig[i] - sig[i-1]) for i in range(1,n) ])
-    # Δymax = np.max([ np.abs(sig[i] - sig[i-1]) for i in range(1,n) ])
-    # Δyavg = np.mean([ np.abs(sig[i] - sig[i-1]) for i in range(1,n) ])
-
-    # sigmax = np.max(sig)
-    # sigmin = np.min(sig)
-    # print("ymax step:", Δymax)
-    # print("ymin step:", Δymin)
-    # print("yavg step:", Δyavg)
-    # print("sigmax: ", sigmax)
-    # print("sigmin:", sigmin)
-
 
 detectPQRSTf(xca)
 detectPQRSTf(xnorm)
