36c46bfb8ee2da60dfdf4f94da2b78c219935daa
eunicen1 <ee.nwaobi@gmail.com>
2021-11-23 19:05:28 -0800

    maxpeakdetector needs fixing

--- PQRSTf.py
+++ PQRSTf.py
@@ -4,12 +4,14 @@ import wfdb
 import matplotlib.pyplot as plt
 from wfdb import processing as prc
 
-record_ca = wfdb.rdrecord('mitbih/420') 
-record_normal = wfdb.rdrecord('ecgiddb/Person_01/rec_1')
-print(record_ca.__dict__['fs'])
-print(record_ca.__dict__['p_signal'])
+name_ca = 'mitbih/420'
+name_norm = 'ecgiddb/Person_01/rec_1'
+record_ca = wfdb.rdrecord(name_ca) 
+record_norm = wfdb.rdrecord(name_norm)
+print('frequency of signal '+ name_ca, ': ', record_ca.__dict__['fs'])
+print('frequency of signal '+ name_norm, ': ', record_norm.__dict__['fs'])
 xca = record_ca.__dict__['p_signal'][:, 1]
-xnorm = record_normal.__dict__['p_signal'][:, 1]
+xnorm = record_norm.__dict__['p_signal'][:, 1]
 
 #Runtime below: ~O(n log n + n) = ~O(n log n)
 
@@ -22,9 +24,11 @@ def pkRatio(arr, p=0.33):
     Δp = int(p*n)
     arr2 = np.copy(arr[:,1])
     arr2.sort(kind='mergesort') # sorts array in place
+    print(arr2, arr2[n-Δp-1:])
     avgppeak = np.mean(arr2[n-Δp-1:])
     #determine ratio of peaks 
     ratios = 100*arr[:,1]/avgppeak
+    print(ratios)
     return np.concatenate((arr, ratios.reshape(n,1)), axis=1)
 
 #Input = 1-d signal array
@@ -59,14 +63,14 @@ def detectPQRSTf(sig):
     maxs = np.array(maxs)
 
     # all maxs and relative closeness to max
-    rmaxs = pkRatio(maxs)#, p=float((maxs.shape[0])**(-1)))
+    rmaxs = pkRatio(maxs, p=0.40)#, p=float((maxs.shape[0])**(-1)))
     r, _ = rmaxs.shape
 
     #get estimated QRS peaks
     QRS = []
     for i in range(r):
         percent = rmaxs[i,2] # obtain percentage %
-        if int(percent) >= 100:
+        if int(percent) >= 60:
             QRS.append([rmaxs[i,0], rmaxs[i,1], i])
     QRS = np.array(QRS)
     s, _ = QRS.shape
@@ -75,12 +79,13 @@ def detectPQRSTf(sig):
     P = []
     T = []
     QRSnew = []
-    for i in range(1,s-2):
+    for i in range(1,s-2, 2):
+        # print(i-1, i, i+1)
         startP = int(QRS[i-1,2])
         stopP = int(QRS[i,2])
         startT = int(QRS[i,2])
         stopT = int(QRS[i+1,2])
-        print(startP, stopP, startT, stopT)
+        # print(rmaxs[startP,0], rmaxs[stopP,0], rmaxs[stopT,0])
         sliceP = rmaxs[startP+1:stopP,:] #get Pzone (peaks)
         sliceT = rmaxs[startT+1:stopT,:] #get Tzone (peaks)
     
@@ -89,25 +94,28 @@ def detectPQRSTf(sig):
             QRSnew.append(QRS[i,:])
             P.append([sliceP[np.argmax(sliceP[:,1]),0], np.max(sliceP[:,1])])
             T.append([sliceT[np.argmax(sliceT[:,1]),0], np.max(sliceT[:,1])])
-
+    
     P = np.array(P)
     T = np.array(T)
     QRS = np.array(QRSnew)
-    print(P.shape, T.shape, QRS.shape)
-    #[mean frequency of P-wave, mean frequency of QRS wave, mean frequency of T wave]    
+
+    Pamplitude = np.mean(P[:,1])
+    QRSamplitude = np.mean(QRS[:,1])
+    Tamplitude = np.mean(T[:,1])
+    
+    #[mean frequency of P wave, mean frequency of QRS wave, mean frequency of T wave]    
     fs = [np.mean(np.diff(P[:,0])), np.mean(np.diff(QRS[:,0])), np.mean(np.diff(T[:,0]))]
-    print("frequencies of P, QRS, and T", fs)
-    f = np.mean(fs) #?
+    f = n/np.mean(fs)
     
     np.savetxt("neginds.csv", neginds, delimiter=",")
     plt.plot(sig)
     plt.scatter(neginds[:,0], neginds[:,1], color='cyan')
-    # plt.scatter(maxs[:,0], maxs[:,1], color='coral')
     plt.scatter(QRS[:,0], QRS[:,1], color='maroon')
     plt.scatter(P[:,0], P[:,1], color='lime')
     plt.scatter(T[:,0], T[:,1], color='red')
     plt.show()
-    
 
-detectPQRSTf(xca)
-detectPQRSTf(xnorm)
+    return Pamplitude, QRSamplitude, Tamplitude, f, fs
+
+print(detectPQRSTf(xca))
+print(detectPQRSTf(xnorm))
--- extra.py
+++
@@ -1,4 +0,0 @@
-print("max: ", np.max(sliceP[:,1]), 
-            # "index of max: ", sliceP[np.argmax(sliceP[:,1]),0],
-            # "signal @ index above ^: ", sig[int(sliceP[np.argmax(sliceP[:,1]),0])] 
-            # )
